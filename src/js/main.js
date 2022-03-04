6e5de7f13e1aa608d2437f8b6e3c4859081024e9
hori-ak <44099134+hori-ak@users.noreply.github.com>
2022-03-04 14:04:01 +0900

    20220304_Smoothscrollのライブラリ変更

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@
 import "./module/webFont";
 import "./module/setAnimation";
 import "./module/fixViewport";
-import smoothscroll from "smoothscroll-polyfill";
-import Swiper, { Navigation, Pagination, Autoplay,Scrollbar } from 'swiper';
+import SmoothScroll from "smooth-scroll";
+/*import Swiper, { Navigation, Pagination, Autoplay,Scrollbar } from 'swiper';
 
 const swiper = new Swiper(".mySwiper", {
 	modules: [Navigation, Pagination, Autoplay,Scrollbar],
@@ -23,29 +23,8 @@ const swiper = new Swiper(".mySwiper", {
       spaceBetween: 30
     }
   }
-});
+});*/
 
-// kick off the polyfill!
-smoothscroll.polyfill();
-
-document.addEventListener("click", e => {
-  const target = e.target;
-
-  if (target.href.match(/#/)) {
-    e.preventDefault();
-    const targetId = target.hash;
-    const targetElement = document.querySelector(targetId);
-    // 画面上部から要素までの距離
-    const rectTop = targetElement.getBoundingClientRect().top;
-    // 現在のスクロール距離
-    const offsetTop = window.pageYOffset;
-    // スクロール位置に持たせるバッファ
-    const buffer = 50;
-    const top = rectTop + offsetTop - buffer;
-
-    window.scrollTo({
-      top,
-      behavior: "smooth"
-    });
-  }
+const scroll = new SmoothScroll('a[href*="#"]',{
+  header: '[data-scroll-header]'
 });
