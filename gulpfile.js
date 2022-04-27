078eecf1dc4a8589e08a20cbe72fe46321a79a7b
hori-ak <44099134+hori-ak@users.noreply.github.com>
2022-04-27 21:13:54 +0900

    テンプレートとしてEJSを導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 const gulp = require("gulp")
-const gulpDartSass = require("gulp-dart-sass")
-const sassGlob = require("gulp-sass-glob-use-forward")
-const autoprefixer = require("gulp-autoprefixer")
-const sourcemaps = require("gulp-sourcemaps")
-const cleanCSS = require("gulp-clean-css");
-const mediaQueries = require("gulp-group-css-media-queries")
+
+//開発モードと本番モード
 const mode = require("gulp-mode")({
   modes: ["production", "development"],
   default: "development",
   verbose: false,
-});
+})
+
+//sass用パッケージの読込み
+const gulpDartSass = require("gulp-dart-sass")
+const sassGlob = require("gulp-sass-glob-use-forward")
+const autoprefixer = require("gulp-autoprefixer")
+const sourcemaps = require("gulp-sourcemaps")
+const cleanCSS = require("gulp-clean-css")
+const mediaQueries = require("gulp-group-css-media-queries")
 
 const paths = {
   srcDir: "./src/scss",
   dstDir: "./dist/css",
-};
+}
 
+//sassの処理
 const sass = () => {
   return gulp
     .src(paths.srcDir + "/*.scss")
@@ -32,3 +37,18 @@ const sass = () => {
     .pipe(gulp.dest(paths.dstDir))
 }
 exports.sass = sass
+
+//EJS用パッケージの読込み
+const rename = require("gulp-rename")
+const ejs = require("gulp-ejs")
+const replace = require("gulp-replace")
+
+//EJSの処理
+gulp.task("ejs", (done) => {
+	gulp
+		.src(["ejs/**/*.ejs", "!" + "ejs/**/_*.ejs"])
+		.pipe(ejs({}, {}, {ext:'.html'}))
+		.pipe(rename({ extname: ".html" }))
+		.pipe(gulp.dest("./"))
+	done()
+})
