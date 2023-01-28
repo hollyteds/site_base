90a62852d137356d04cb8f2c4553206817e88350
hori-ak <t-horiuchi@aoba-koukoku.co.jp>
2023-01-28 14:47:55 +0900

    accordion.ksのエラー処理追記

--- src/js/module/accordion.js
+++ src/js/module/accordion.js
@@ -1,8 +1,17 @@
 /**
  * Accordion
+ * AcDatasの要素にはスタイルを設定しないこと
+ * paddingとかのスタイルはその子要素に設定する
  */
 
 export default class {
+  /**
+   * 
+   * @param {*} AcBtns 
+   * 発火対象のelement 
+   * @param {*} AcDatas
+   * 開閉させるエレメント、
+   */
   constructor(AcBtns, AcDatas) {
     this.isResize = true;
     this.w = window.innerWidth;
@@ -10,23 +19,33 @@ export default class {
     this.AcBtns = AcBtns;
     this.AcDatas = AcDatas;
 
+    //アコーディオン対象が１つも取得できない場合は実行しない
+    if (AcBtns.length === 0 || AcDatas.length === 0) return;
+
     this.onSwitch = this.onSwitch.bind(this);
     this.init();
   }
 
+  /**
+   * アコーディオン対象のElmentの初期スタイル
+   */
   setStyle() {
     this.AcDatas.forEach(ele => {
       let h = ele.children[0].clientHeight
       ele.style.height = 0;
-
+      ele.style.overflow = "hidden";
+      ele.style.transition = "height 0.2s";
       this.HeightArray.push(h);
     });
   }
 
+  /**
+   * 全てのボタンを閉じる
+   */
   onAllSwitch() {
     this.AcBtns.forEach(ele => {
-      var parent = ele.parentNode;
-      var next = ele.nextElementSibling;
+      const parent = ele.parentNode;
+      const next = ele.nextElementSibling;
       if (parent.classList.contains("is-open")) {
         next.style.height = 0;
         parent.classList.remove("is-open");
@@ -34,9 +53,14 @@ export default class {
     });
   }
 
+  /**
+   * ボタンを押した時の処理
+   * @param {*} ele 
+   * @param {*} i 
+   */
   onSwitch(ele, i) {
-    var parent = ele.parentNode;
-    var next = ele.nextElementSibling;
+    const parent = ele.parentNode;
+    const next = ele.nextElementSibling;
 
     if (parent.classList.contains("is-open")) {
       this.onAllSwitch();
@@ -49,6 +73,9 @@ export default class {
     }
   }
 
+  /**
+   * リサイズされた時の処理
+   */
   onResize() {
     this.HeightArray.length = 0;
 
@@ -60,6 +87,9 @@ export default class {
     this.w = window.innerWidth;
   }
 
+  /**
+   * 初期処理
+   */
   init() {
     this.setStyle();
 
@@ -80,5 +110,9 @@ export default class {
         this.onSwitch(ele, i);
       });
     });
+
+    // 一つ目のアコーディオンを開いておく
+    this.onSwitch(this.AcBtns[0], 0);
   }
+
 }
