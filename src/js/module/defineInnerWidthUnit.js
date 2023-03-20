8513d74e559a97bcc410a67b37b538cab84db2ee
hori-ak <t-horiuchi@aoba-koukoku.co.jp>
2023-03-20 16:05:27 +0900

    defineInnerWidthUnitモジュールを追加

---
+++ src/js/module/defineInnerWidthUnit.js
@@ -0,0 +1,11 @@
+/**
+ * スクロールバーを除いた幅を1/100の単位にするモジュール
+ * スクロールバーを除いた幅を取得、CSS変数で出力する
+ */
+
+export default (variableName) => {
+	const headerWidth = document.querySelector('.l-header').clientWidth;
+	const innerWidth = headerWidth / 100
+	const rootDocument = document.documentElement;
+	rootDocument.style.setProperty(`--${variableName}`, `${innerWidth}px`);
+}
